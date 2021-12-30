51afb0fc062fd14e448fbe9ad1c14f58b14458e2
giangcs <giang.chuson.ncc@gmail.com>
2021-12-30 09:00:08 +0700

    add image and reaction

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,20 +4,12 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Image;
-use Carbon\Carbon;
-use DateTime;
 
 class MainController extends Controller
 {
     public function show()
     {
         $images = Image::with('reactions')->orderBy('createdTimestamp')->paginate(50);
-//        foreach ($images as $image){
-//            var_dump($image->createdTimestamp);
-//            var_dump((new DateTime())->setTimestamp($image->createdTimestamp->__toString() / 1000)->format('d/m/Y H:i'));
-
-//            var_dump((new DateTime())->setTimestamp( 1640623557301 / 1000)->format('d/m/Y H:i'));
-//        }
         return view('main', compact('images'));
     }
 }
